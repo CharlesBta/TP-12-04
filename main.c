10cf4fe785d252549335d4eab0ebc2b2633b80b6
Charles-Antoine <charto.bta@gmail.com>
2024-04-12 11:53:41 +0200

    v0.2

--- main.c
+++ main.c
@@ -37,9 +37,9 @@ Line ajouterLigne(int listeCorrespondences[], int nbCorrespondences) {
 }
 
 void GPS(Line depart, Line arrive) {
-    int nbArrets;
-    for (int i = depart.correspondences[0]; i < 3; i = depart.correspondences[i+1]) {
-//        printf("%d\n",i);
+    int index = 0;
+    int i = depart.correspondences[0];
+    while (index < 3){
         bool found = false;
         for (int j = 0; j < 3; ++j) {
             if (found)
@@ -52,7 +52,9 @@ void GPS(Line depart, Line arrive) {
                 return;
             } else {
                 for (int k = lignes[i].correspondences[0]; k < 3; k = lignes[i].correspondences[k+1]) {
-
+//                    printf("k: %d\n", k);
+                    if (k == -1)
+                        break;
                     if (lignes[i].correspondences[k] == arrive.id) {
                         found = true;
                         printf("next: %d\n", lignes[i].id);
@@ -61,14 +63,16 @@ void GPS(Line depart, Line arrive) {
                 }
             }
         }
+        index++;
+        i = depart.correspondences[index];
     }
 }
 
 // on aurait du faire en avec une fonction recusive mais on l'a pas travailler en classe
-// je manque de pratique pour le faire en moins de 2h là 
+// je manque de pratique pour le faire en moins de 2h là
 
 int main() {
-    lignes[0] = ajouterLigne((int[]) {1,2}, 1);
+    lignes[0] = ajouterLigne((int[]) {1, 2}, 2);
     lignes[1] = ajouterLigne((int[]) {0}, 1);
     lignes[2] = ajouterLigne((int[]) {1}, 1);
 
@@ -80,6 +84,8 @@ int main() {
     GPS(lignes[2], lignes[1]);
     printf("\n1->0\n");
     GPS(lignes[1], lignes[0]);
+    printf("\n0->2\n");
+    GPS(lignes[0], lignes[2]);
 
     return 0;
 }
